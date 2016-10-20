f58a55cf503d57197bd7607f995acd6636c68632
Rougin Royce Gutib <rougingutib@gmail.com>
2016-10-20 13:22:28 +0800

    Update code documentation

--- src/Wildfire.php
+++ src/Wildfire.php
@@ -17,10 +17,10 @@ class Wildfire extends \CI_Model
     use Traits\DatabaseTrait, Traits\ObjectTrait, Traits\ResultTrait;
 
     /**
-     * @param \CI_DB|\CI_DB_query_builder|null $database
-     * @param \CI_DB_result|null               $query
+     * @param \CI_DB_query_builder|null $database
+     * @param \CI_DB_result|null        $query
      */
-    public function __construct($database = null, $query = null)
+    public function __construct(\CI_DB_query_builder $database = null, $query = null)
     {
         if (! empty($database)) {
             $this->setDatabase($database);
